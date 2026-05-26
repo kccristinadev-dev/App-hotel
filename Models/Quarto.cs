608c5a2e1e0dcbad7dab585a1bf6a398842ba74a
Cristina <kc.cristinadev@gmail.com>
2026-05-26 18:47:20 -0300

    Add Quarto class with properties for room details

--- Models/Quarto.cs
+++ Models/Quarto.cs
@@ -1,1 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+
+namespace MauiPaaHotel.Models
+{
+public class Quarto {
+
+public String descricao {get; set;}
+public double valorAdulto {get; set;}
+public double valorCrianca {get; set;}
+
+}
+}
