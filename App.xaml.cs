48659fce3c35650f9f17ed34965a386914656a2c
Cristina <kc.cristinadev@gmail.com>
2026-05-26 18:58:00 -0300

    Initialize room list with pricing information
    Added a list of room types with pricing details.

--- App.xaml.cs
+++ App.xaml.cs
@@ -4,6 +4,36 @@ namespace AppHotel
 {
     public partial class App : Application
     {
+
+    List<Quartos> lista_quartos = new List<Quartos>{
+
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.0,
+                ValorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.0,
+                ValorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50,
+                ValorDiariaCrianca = 25
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25,
+                ValorDiariaCrianca = 12.5
+            }
+            
+
+    };
         public App()
         {
             InitializeComponent();
