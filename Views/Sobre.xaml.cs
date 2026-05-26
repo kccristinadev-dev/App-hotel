8f27ffaae696f0ef05f883948bb3b5ea504ed5f4
Cristina <kc.cristinadev@gmail.com>
2026-05-26 17:23:34 -0300

    Atualizar o Sobre.xaml.cs

--- Views/Sobre.xaml.cs
+++ Views/Sobre.xaml.cs
@@ -3,10 +3,10 @@ public reserva(){
 }
 private void voltar(object sender, EventArgs e){
 try{
-
+     Navigation.PopAsync();
 }
 catch (Exeption ex){
-
+ DisplayArlert("Ops ",ex.mensagem, " ok");
 }
 
 }
