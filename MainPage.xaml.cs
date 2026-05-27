3c36aa5fb1377fca451d5cf2667e1f9c16049131
Cristina <kc.cristinadev@gmail.com>
2026-05-27 19:12:48 -0300

    Atualizar o MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -15,4 +15,18 @@ namespace AppHotel
             await Navigation.PushAsync(new Views.HomePage());
         }
     }
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
 }
